857282d90e28826f61b2f966fe55b01d74a135ba
Menshikova <mensikova.anastasija@gmail.com>
2018-07-20 21:17:47 -0700

    CSS generation - generalised

--- table-generation/cssgen.py
+++ table-generation/cssgen.py
@@ -8,6 +8,8 @@ import os
 parameters = {}
 # {'font-family': {'Georgia, serif': '0.02', '"Palatino Linotype", "Book Antiqua", Palatino, serif': '0.02', '"Times New Roman", Times, serif': '0.8', 'Arial, Helvetica, sans-serif': '0.06', '"Arial Black", Gadget, sans-serif': '0.05', '"Comic Sans MS", cursive, sans-serif': '0', 'Impact, Charcoal, sans-serif': '0', '"Lucida Sans Unicode", "Lucida Grande", sans-serif': '0', 'Tahoma, Geneva, sans-serif': '0.01', '"Trebuchet MS", Helvetica, sans-serif': '0', 'Verdana, Geneva, sans-serif': '0.01', '"Courier New", Courier, monospace': '0.03', '"Lucida Console", Monaco, monospace': '0'}, 'font-style': {'normal': '0.9', 'italic': '0.08', 'oblique': '0.02'}, 'font-weight': {'normal': '0.9', 'bold': '0.1'}, 'font-variant': {'normal': '0.85', 'small-caps': '0.15'}, 'font-size': {'8px': '0.1', '10px': '0.3', '12px': '0.2', '14px': '0.1', '16px': '0.1', '18px': '0.1', '20px': '0.1', '22px': '0', '24px': '0', '26px': '0'}, 'width': {'100%': '0.7', '75%': '0.2', '50%': '0.1'}, 'height': {'100px': '0.05', '500px': '0.15', '1000px': '0.2', '1500px': '0.2', '2000px': '0.3', '2500px': '0.1', '3000px': '0', '4000px': '0', '5000px': '0'}, 'padding': {'5px': '0.6', '10px': '0.2', '15px': '0.1', '20px': '0.1'}, 'border-collapses': {'yes': '0.3', 'no': '0.7'}, 'border thickness': {'1px': '0.7', '2px': '0.2', '3px': '0.1', '4px': '0'}, 'border type': {'solid': '0.95', 'dotted': '0.05'}, 'border color': {'black': '0.9', 'blue': '0.1', 'red': '0'}, 'border-bottoms': {'yes': '0.2', 'no': '0.8'}, 'text-align': {'left': '0.4', 'right': '0.4', 'center': '0.2'}, 'vertical-align': {'top': '0.4', 'bottom': '0.4', 'middle': '0.2'}, 'character distributions': {'words': '0.5', 'numbers': '0.4', 'symbols': '0.1'}}
 num_of_tables = 436700160
+num_of_files = 0
+
 
 def build_json(index, lth, lty, lclr):
     """
@@ -142,9 +144,52 @@ def write_to_file(num_of_params, pars, nums, param_name, css_arr):
     counter = 0
     for a in range(num_of_params): #num_of_params
         for i in range(nums[a]): #nums[a]
+            if counter >= num_of_files:
+                continue
             css_arr[counter] += "\n\t" + param_name + ": " + pars[a] + ";"
             print("COUNTER: " + str(counter))
             counter += 1
+    return css_arr
+
+# tag = "p"
+# pars = ["font-family", "font-style",...]
+# section = "font-"
+# num_of_pars = [13, 3, 2, 2, 10]
+def general_table_css_generator(tag, pars, section, num_of_pars):
+    if section == 'border ':
+        sys.exit("Border settings require a separate function!")
+    list_of_nums = []
+    list_of_pars = []
+    for p in pars: #font-family
+        l_n = []
+        l_p = []
+        for par in parameters: #font-family
+            for pr in parameters[par]: # Georgia,serif
+                if par != p:
+                    continue
+                if par == p:
+                    l_p.append(pr)
+                    l_n.append(int(num_of_tables * float(parameters[par][pr])))
+        list_of_pars.append(l_p)
+        list_of_nums.append(l_n)
+    css_arr = []
+    path = './CSS_test/'
+    global num_of_files
+    for filename in os.listdir(path):
+        css_arr.append("p {")
+        num_of_files += 1
+    for el in range(len(pars)):
+        write_to_file(num_of_pars[el], list_of_pars[el], list_of_nums[el], pars[el], css_arr)
+    for c in range(len(css_arr)):
+        css_arr[c] += "\n}\n"
+    print("ARR LENGTH: " + str(len(css_arr)))
+    print("NUM OF TABLES: " + str(num_of_tables))
+    for el in range(len(css_arr)):
+        print(css_arr[el])
+        with open("./CSS_test/" + str(el) + ".css", "a") as f:
+            f.write(css_arr[el])
+
+
 
 def fonts():
     nums_ff = []
@@ -180,9 +225,11 @@ def fonts():
     print(nums_ff)
     css_arr = []
     path = './CSS_test/'
+    global num_of_files
     for filename in os.listdir(path):
         css_arr.append("p {")
-    print(css_arr)
+        num_of_files += 1
+    # print(css_arr)
     write_to_file(13, ff, nums_ff, "font-family", css_arr)
     write_to_file(3, fst, nums_fst, "font-style", css_arr)
     write_to_file(2, fw, nums_fw, "font-weight", css_arr)
@@ -191,13 +238,22 @@ def fonts():
     for c in range(len(css_arr)):
         css_arr[c] += "\n}\n"
     print("ARR LENGTH: " + str(len(css_arr)))
+    print("NUM OF TABLES: " + str(num_of_tables))
     for el in range(len(css_arr)):
-        f = open("./CSS_test/" + str(el) + ".css", "a")
-        f.write(css_arr[el])
-        f.close()
+        print(css_arr[el])
+        with open("./CSS_test/" + str(el) + ".css", "a") as f:
+            f.write(css_arr[el])
+        # f = open("./CSS_test/" + str(el) + ".css", "a")
+        # f.write(css_arr[el])
+        # f.close()
 
 
 load_params()
 
 # borders()
-fonts()
+# fonts()
+
+# tag = "p"
+# pars = ["font-family", "font-style",...]
+# section = "font-"
+general_table_css_generator("p", ["font-family", "font-style", "font-weight", "font-variant", "font-size"], "font-", [13, 3, 2, 2, 10])
